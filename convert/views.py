9fa48556fee426c6e49acf3b13d3f5f1fecbf786
skconan <supakit.kr@gmail.com>
2019-05-20 13:25:43 +0700

    add all

---
+++ convert/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from gtts import gTTS
+from django.conf import settings
+
+# Create your views here.
+def convert(req):
+    if req.method == 'POST':
+        text = req.POST["text"].split("\r\n")
+        result = []
+        for t in text:
+            tts = gTTS(t)
+            t = t.replace('?','')
+            t = t.replace('.','')
+            t = t.replace('"','')
+            t = t.replace("'",'')
+            tts.save(settings.MEDIA_ROOT + "\\" + t+'.mp3')
+            result.append([t,settings.MEDIA_URL + "\\" + t+'.mp3'])
+        context = {"result":result}
+        template = 'download.html'
+        return render(req, template, context)
+    context = {}
+    template = 'convert.html'
+    return render(req, template, context)
